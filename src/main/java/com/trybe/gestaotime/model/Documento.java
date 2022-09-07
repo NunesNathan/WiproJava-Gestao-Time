771626ea2a4c43be11aa661d9d745eb8561c3e98
NunesNathan <nunesxd7@hotmail.com>
2022-09-07 20:05:15 -0300

    feat: documento entity without relationship

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,16 +1,62 @@
 package com.trybe.gestaotime.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Classe Documento.
  */
 
+@Entity
+@Table(name = "Documento")
 public class Documento {
   /**
    * Atributos.
    */
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @Column(name = "numeroCarteiraTrabalho", nullable = false)
+  private String numeroCarteiraTrabalho;
+  @Column(name = "cpf", nullable = false)
+  private String cpf;
+  @Column(name = "numeroCbf", nullable = false)
+  private String numeroCbf;
 
   /**
    * Métodos.
    */
+  public Long getId() {
+    return id;
+  }
+
+  public String getNumeroCarteiraTrabalho() {
+    return numeroCarteiraTrabalho;
+  }
+
+  public void setNumeroCarteiraTrabalho(String numeroCarteiraTrabalho) {
+    this.numeroCarteiraTrabalho = numeroCarteiraTrabalho;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNumeroCbf() {
+    return numeroCbf;
+  }
+
+  public void setNumeroCbf(String numeroCbf) {
+    this.numeroCbf = numeroCbf;
+  }
 
 }
