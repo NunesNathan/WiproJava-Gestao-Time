db1c11093c568b48019bd913200eb859f028c4e6
NunesNathan <nunesxd7@hotmail.com>
2022-09-08 11:57:53 -0300

    feat: torcedor entity

--- src/main/java/com/trybe/gestaotime/model/Torcedor.java
+++ src/main/java/com/trybe/gestaotime/model/Torcedor.java
@@ -1,7 +1,11 @@
 package com.trybe.gestaotime.model;
 
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 /**
@@ -14,11 +18,37 @@ public class Torcedor {
   /**
    * Atributos.
    **/
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "nome", nullable = false)
+  private String nome;
+
   @ManyToMany(mappedBy = "times")
   private List<Time> times;
 
   /**
    * Métodos.
    **/
+  public Long getId() {
+    return id;
+  }
 
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Time> getTimes() {
+    return times;
+  }
+
+  public void setTimes(List<Time> times) {
+    this.times = times;
+  }
 }
